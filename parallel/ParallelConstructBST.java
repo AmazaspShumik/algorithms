9c4f783c247f2b712cba90235c879d95413c1e7a
amazaspshaumyan <sha-amazasp@yandex.ru>
2015-04-28 21:27:51 -0700

    Fixed bug for arrays of size one

--- parallel/ParallelConstructBST.java
+++ parallel/ParallelConstructBST.java
@@ -8,6 +8,11 @@ package com.algorithms.parallel;
  * advantage of several processors (for small inputs program will
  * still be using only single thread)
  *
+ * Perfomance:
+ *            Work = O(n)
+ *            Span = O(log(n))
+ *            Parallelism = O(n/log(n)) (exponential parallelism)
+ *
  * In solution we used two simple helper classes : Node and BinarySearchTreeHelper
  *
  */
@@ -31,7 +36,7 @@ class ParallelBuild<T> extends RecursiveTask<Node<T>>{
         if(hi < lo) return null;
         int mid = lo+(hi-lo)/2;
         Node<T> current = new Node<>(x[mid],hi-lo+1);
-        current.left = computeSeq(x, lo, mid);
+        current.left = computeSeq(x, lo, mid-1);
         current.right = computeSeq(x, mid + 1, hi);
         return current;
     }
@@ -43,8 +48,8 @@ class ParallelBuild<T> extends RecursiveTask<Node<T>>{
         }
         int mid = lo+(hi-lo)/2;
         Node<T> current = new Node<>(x[mid], hi-lo+1);
-        ParallelBuild<T> left = new ParallelBuild<>(x,lo,mid);
-        ParallelBuild<T> right = new ParallelBuild<>(x,lo,mid);
+        ParallelBuild<T> left = new ParallelBuild<>(x,lo,mid-1);
+        ParallelBuild<T> right = new ParallelBuild<>(x,mid+1,hi);
         left.fork(); // spawn new thread
         current.right = right.compute(); // process right subtree in current thread
         current.left = left.join(); // after completion spawned thread returns Node (see RecursiveTask class doc )
@@ -68,11 +73,10 @@ public class ParallelConstructBST{
 
 
     public static void main(String[] args){
-        Integer[] x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+        Integer[] x = {1,2,3,4,5,6,7,8,9,10};
         BinarySearchTreeHelper<Integer> bst = construct(x);
-        System.out.println(bst.root);
-        System.out.println(bst.root.left.data);
-        System.out.println(bst.root.right.data);
+        bst.printTree();
+        System.out.println(bst.root.left.right.right.data);
     }
 
 
