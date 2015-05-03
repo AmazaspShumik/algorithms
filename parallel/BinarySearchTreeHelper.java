54d9d3f249cf4ad6398114442e5b73258f4426a5
amazaspshaumyan <sha-amazasp@yandex.ru>
2015-05-03 15:54:44 -0400

    Add method to print tree

--- parallel/BinarySearchTreeHelper.java
+++ parallel/BinarySearchTreeHelper.java
@@ -1,5 +1,7 @@
 package com.algorithms.parallel;
 
+import java.util.Queue;
+import java.util.ArrayDeque;
 
 public class BinarySearchTreeHelper<T>{
 
@@ -7,6 +9,17 @@ public class BinarySearchTreeHelper<T>{
 
     public BinarySearchTreeHelper(Node<T> root){
         this.root = root;
+    }
+
+    public void printTree(){
+        Queue<Node<T>> q = new ArrayDeque<Node<T>>();
+        q.add(root);
+        while(!q.isEmpty()){
+            Node<T> x = q.poll();
+            System.out.print(x.data.toString()+" ");
+            if(x.left!=null) q.add(x.left);
+            if(x.right!=null) q.add(x.right);
+        }
     }
 
 }
